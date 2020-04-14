0c525b22f1917d1bd77136ad50116f2cee58eca2
50964106 <i.pellarp@gmail.com>
2020-04-14 11:31:22 +0200

    Init changes

--- MVC2-Lab1/Startup.cs
+++ MVC2-Lab1/Startup.cs
@@ -23,6 +23,7 @@ namespace MVC2_Lab1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddControllersWithViews();
         }
 
